2ac5bf78bac0702d58bccc19d3e267e8b7d4928a
bysabbir <sabbir.ahmed@sslwireless.com>
2020-04-22 23:55:31 +0600

    done lesson3

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace GradeBook
+namespace Program
 {
-    class Program
+    class GradeBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numbers = new double[] {12.1, 11.2, 23.2, 45.4};
+
+            // generic list
+            List<double> grades = new List<double>() {12.1, 11.2, 23.2, 45.5};
+            grades.Add(56.1);
+
+            double result = 0;
+            // for (var i = 0; i < numbers.Length; i++){
+            //     result = result + numbers[i];
+            // }
+            foreach (var number in grades)
+            {
+                result += number;
+            }
+            var avg = result / grades.Count;
+            Console.WriteLine(avg);
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello {args[0]}");
+            }
+            else
+            {
+                Console.WriteLine("Hello World!");            
+            }
         }
     }
 }
