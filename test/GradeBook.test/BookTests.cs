e62226df029252c182f256910fee9eecb6075887
bysabbir <sabbir.ahmed@sslwireless.com>
2020-04-25 03:07:53 +0600

    lesson 5 done

---
+++ test/GradeBook.test/BookTests.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+
+namespace GradeBook.test
+{
+    public class BookTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            // arrange
+            var book = new Book("");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.3);
+
+            // act
+            var stat = book.GetStats();
+
+            // assert
+            Assert.Equal(85.6, stat.Average, 1);
+            Assert.Equal(77.3, stat.Low);
+            Assert.Equal(90.5, stat.High);
+        }
+    }
+}
